a939f75d26ab426f219c523449d1b9712a8debc7
Bhawesh Goswami <goswamibhawesh95@gmail.com>
2026-05-17 00:40:36 +0530

    Update day1.py

--- Desktop/ds journey/day1.py
+++ Desktop/ds journey/day1.py
@@ -1,1 +1,3 @@
 print("data science journey")
+print("im beginner in data science i want to ace this field")
+print("my plans are simple to clear this ds jiurney crack gate and get best research institue")
