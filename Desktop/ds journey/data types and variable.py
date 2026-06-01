46cb7dc1677694c57c2dcd4d72bfe5fa6d592220
Bhawesh Goswami <goswamibhawesh95@gmail.com>
2026-06-02 01:38:38 +0530

    Update data types and variable.py

--- Desktop/ds journey/data types and variable.py
+++ Desktop/ds journey/data types and variable.py
@@ -1,1 +1,10 @@
 
+# data types and variables task
+
+coustmer_name = "Bhawesh"                                        
+coffee_count = 4
+coffee_price = 4.12
+has_discount = True
+
+total_price = coffee_count * coffee_price
+print(f"Hi {coustmer_name}, your totaal today is {total_price}!")
