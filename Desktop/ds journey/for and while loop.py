ac48436a6fb9673d557b8a3a16abc70afe6f40fd
Bhawesh Goswami <goswamibhawesh95@gmail.com>
2026-06-05 00:19:33 +0530

    Update for and while loop.py

--- Desktop/ds journey/for and while loop.py
+++ Desktop/ds journey/for and while loop.py
@@ -1,1 +1,15 @@
 
+ # loops
+# Printing a countdownammo = 3
+
+# # Keep shooting as long as we have ammo
+# for count in range(3):
+#     print("Jumping jack!")
+
+# for count in range(10):
+#     print("my name is Bhawesh")
+#     print(" im an idiot")
+#     x = 1
+#     y = x + 3
+#     print(y)
+#     print(f"{y} heyaa this thing works")
