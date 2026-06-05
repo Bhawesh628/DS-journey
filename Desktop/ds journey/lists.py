144d3b6191848d542525f9b7fb0d50b17ed7d7ea
Bhawesh Goswami <goswamibhawesh95@gmail.com>
2026-06-06 00:02:45 +0530

    Update lists.py

--- Desktop/ds journey/lists.py
+++ Desktop/ds journey/lists.py
@@ -6,3 +6,12 @@ mylist.append(3)
 print(mylist[0])
 print(mylist[1])
 
+
+grocerylist = []
+grocerylist.append("mango")
+grocerylist.append("banana")
+grocerylist.append("papaya")
+
+print(grocerylist[0])
+print(grocerylist[1])
+print(grocerylist[2])
