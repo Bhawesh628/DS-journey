327d491351c73cc6ae95564acfe2c9b8d4fd746d
Bhawesh <goswamibhawesh95@gmail.com>
2026-06-07 00:09:19 +0530

    Add basic operators examples in Python
    Added examples of modulus, exponentiation, and string concatenation.

--- Desktop/ds journey/basic operators.py
+++ Desktop/ds journey/basic operators.py
@@ -4,3 +4,22 @@ x2 = 2*2
 x3 = 2/2
 x4 = 5-3
 print(x1,x2,x3,x4)
+
+x = 20 % 10
+print(x)
+double *  symbol makes a power relationship 
+y = 2**3
+print(y)
+y2 = 3**3
+print(y2)
+y3 = 3**21
+print(y3)
+
+python supports concatenating strings using the addition operator
+x = "hello " + "" "world"
+print(x)
+y = ("hello world")
+print(y)
+
+lotsofhellos = "hello " * 3
+print(lotsofhellos)
