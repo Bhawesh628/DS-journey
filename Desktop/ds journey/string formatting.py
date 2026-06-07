94b0802e58878d6637c3c575f5cc33599b2a602c
Bhawesh <goswamibhawesh95@gmail.com>
2026-06-08 00:45:10 +0530

    Update string formatting.py

--- Desktop/ds journey/string formatting.py
+++ Desktop/ds journey/string formatting.py
@@ -23,3 +23,25 @@ code = "palantir"
 data = 20
 print(" %s is thrifting %d million user data" %(code,data) )
 
+ You will need to write a format string which prints out the data using the following 
+# syntax: Hello John Doe. Your current balance is $53.44.
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
+
+data = "john doe", 55.44
+format_string = "hello %s your current balance is %s"
+print(format_string %data)
+mylist = [1,2,3]
+print("A list %s" % mylist)
+
+number = 123
+print(" this is just a learmimg %s" %number)
+
+collection = [2,4,6,8]
+print(" these are even numbers: %s" %collection)
+
+collection = [3,5,7,9]
+print(" these are collection of odd numbers %s" %collection)
