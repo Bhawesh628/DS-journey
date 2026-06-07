b951dc9caebb30d43ac0495bba5aaf0b5fbbcce1
Bhawesh <goswamibhawesh95@gmail.com>
2026-06-08 00:44:08 +0530

    Add string formatting examples in Python

--- Desktop/ds journey/string formatting.py
+++ Desktop/ds journey/string formatting.py
@@ -1,1 +1,25 @@
 
+
+name = "Bhawesh"
+print(" Hello  %s" %name)
+
+code = "black cat"
+print(" activate 001 %s" %code)
+
+instruction = " please stay on your seats."
+print("all the flight members, %s"  %instruction)
+
+
+name = "bhawesh"
+print(" hello this is private server, Your welcome %s" %name )
+
+print(" and here you are safe Mr. %s" %name)
+
+name = "John"
+age = 22
+print(" %s is %d year old" % (name ,age))
+
+code = "palantir"
+data = 20
+print(" %s is thrifting %d million user data" %(code,data) )
+
