d3ab6541cf57cdd7d1b514b5d43bb5b00e79c289
Bhawesh <goswamibhawesh95@gmail.com>
2026-06-11 19:34:43 +0530

    Add is_even function to check even numbers

--- Desktop/ds journey/functions.py
+++ Desktop/ds journey/functions.py
@@ -8,3 +8,9 @@ greet_user()
 def homie_calling():
     print("yo homies assemble")
 homie_calling() 
+
+def is_even(number):
+    "return True if the number is even, false otherwise"
+    return number % 2 == 0
+print(is_even(4))
+print(is_even(7))
