0e12799a14addaea9b1f409e62776189aab9fbe4
Bhawesh <goswamibhawesh95@gmail.com>
2026-06-12 20:13:49 +0530

    Update functions.py

--- Desktop/ds journey/functions.py
+++ Desktop/ds journey/functions.py
@@ -14,3 +14,17 @@ def is_even(number):
     return number % 2 == 0
 print(is_even(4))
 print(is_even(7))
+
+def calculate_total_bill(subtotal, tip_percentage , promo_code):
+  tip_amount = subtotal * (tip_percentage/100)
+  discount = 0 
+  if promo_code == "save10":
+    discount = 10
+  elif promo_code == "welcome5":
+    discount = 5
+ final_total = subtotal + tip_amount - discount
+  return final_total
+bill_one = calculate_total_bill(50, 15, "save10")
+print("first_bill_total:" , bill_one)
+bill_two = calculate_total_bill(100, 15, "welcome5")
+print("secod_bill_total:", bill_two)
